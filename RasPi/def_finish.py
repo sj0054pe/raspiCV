8ab8aa67ec0574e9e4a42d1a5a044207521fec97
sj0054pe <mayo0323@gmail.com>
2019-11-28 14:47:10 +0900

    まとめきれていなかった.removeも統合

--- RasPi/def_finish.py
+++ RasPi/def_finish.py
@@ -4,5 +4,6 @@ def organize(fname):
     os.remove('../../%s' % fname)
     os.remove('../../Green.png')
     os.remove('../../Contours_on_%s' % fname)
+    os.remove('../../pre_Area_%s' % fname)
     os.remove('../../Area_%s' % fname)
     print('不必要な画像を削除します。')
