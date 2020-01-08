8fd8fc77ee5b01f7886b6c1d218e06e0153fc2f5
sj0054pe <mayo0323@gmail.com>
2020-01-08 20:41:01 +0900

    ディレクトリ名の変更

--- RasPi/def_datetime.py
+++ RasPi/def_datetime.py
@@ -4,7 +4,7 @@ def check_the_date(Season, RasPi_SerialNum):
     dates=datetime.datetime.now()
     exept_microsec=dates.strftime("%Y-%m-%d-%H-%M")
     print('[Today : %s]' % str(exept_microsec))
-    #fname=RasPi_SerialNum+"_"+Season+"_"+exept_microsec+'.png'
+    fname=RasPi_SerialNum+"_"+Season+"_"+exept_microsec+'.png'
     theDate=dates.strftime("%Y-%m-%d")
-    fname=RasPi_SerialNum+"_"+Season+"_"+theDate+'.png'
+    #fname=RasPi_SerialNum+"_"+Season+"_"+theDate+'.png'
     return fname, theDate
