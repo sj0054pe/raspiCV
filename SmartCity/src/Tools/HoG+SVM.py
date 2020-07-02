e3ea36e6a2afc0dd870d437bfad5ad63829f476c
sj0054pe <mayo0323@gmail.com>
2020-07-03 01:01:21 +0900

    補足説明を追加

---
+++ SmartCity/src/Tools/HoG+SVM.py
@@ -0,0 +1,101 @@
+import numpy as np
+import cv2
+import time
+
+#参照
+#https://qrunch.net/@Atom/entries/5lm14eJkJxBG7OIC
+
+path_input="../../Assets/Assets_Input/"
+fname_input="768x576.avi"
+
+#--------------------------------------------------------------------------------------------
+# HoG特徴量の計算
+hog = cv2.HOGDescriptor()
+# 全身の人を検出(SVM)
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+hogParams = {'winStride': (8, 8), 'padding': (32, 32), 'scale': 1.1}
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+# Lucas-Kanade法のパラメータ #mayoメモ 各画素がどこに移動していたのか表示するため
+#説明サイト https://www.slideshare.net/hitoshinishimura75/lucas-kanade
+lk_params = dict( winSize  = (15,15),
+                  maxLevel = 2,
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+# ランダムに色を100個生成（値0～255の範囲で100行3列のランダムなndarrayを生成）
+color = np.random.randint(0, 255, (100, 3))
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+# サンプル画像
+cap = cv2.VideoCapture('%s%s' % (path_input, fname_input))
+#print('------')
+#print(cap.isOpened())
+# 最初のフレームの処理
+end_flag, frame = cap.read()
+# グレースケール変換
+gray_prev = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# 追跡に向いた特徴
+#--------------------------------------------------------------------------------------------
+# Shi-Tomasiのコーナー検出パラメータ
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7 )
+feature_prev = cv2.goodFeaturesToTrack(gray_prev, mask = None, **feature_params)
+# 元の配列と同じ形にして0を代入
+mask = np.zeros_like(frame)
+#--------------------------------------------------------------------------------------------
+while(end_flag):
+    # グレースケールに変換
+    gray_next = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #時間取得
+    start = time.time()
+    # 人を検出した座標
+    human, r = hog.detectMultiScale(frame, **hogParams)
+    #時間取得
+    end = time.time()
+    # 検出時間を表示
+#    print("{} : {:4.1f}ms".format("detectTime", (end - start) * 1000))
+
+    # オプティカルフロー検出
+    # オプティカルフローとは物体やカメラの移動によって生じる隣接フレーム間の物体の動きの見え方のパターン
+    #mayoメモ レーザを使ったコントラストの効いた画像には不向き(追跡する物体の輝度は不変が前提条件だから)。一方、一般のビデオカメラなどによる動画像への適用性は高いと言えそうです。ロボットビジョンなどでも多用されている理由はこのあたりにあるのかもしれません。
+    feature_next, status, err = cv2.calcOpticalFlowPyrLK(gray_prev, gray_next, feature_prev, None, **lk_params)
+    # オプティカルフローを検出した特徴点を選別（0：検出せず、1：検出した）
+    good_prev = feature_prev[status == 1]
+    good_next = feature_next[status == 1]
+
+    # オプティカルフローを描画
+    for i, (next_point, prev_point) in enumerate(zip(good_next, good_prev)):
+        prev_x, prev_y = prev_point.ravel()
+        next_x, next_y = next_point.ravel()
+        mask = cv2.line(mask, (next_x, next_y), (prev_x, prev_y), color[i].tolist(), 2)
+        frame = cv2.circle(frame, (next_x, next_y), 5, color[i].tolist(), -1)
+
+    svm_img = cv2.add(frame, mask)
+
+    # 人検出した数表示のため変数初期化
+    svm_human_cnt = 0
+    # 人検出した部分を長方形で囲う(SVM)
+    for (x, y, w, h) in human:
+        cv2.rectangle(svm_img, (x, y),(x+w, y+h),(0,0,255), 2)
+        svm_human_cnt += 1
+
+    # 人検出した数を表示
+    cv2.putText(svm_img, "Human Cnt:{}".format(int(svm_human_cnt)),(10,550), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
+    #cv2.moveWindow("SVM",650,100) # Window表示位置指定
+    # ウィンドウに表示
+    cv2.imshow('SVM', svm_img)
+
+    # ESCキー押下で終了
+    if cv2.waitKey(30) & 0xff == 27:
+        break
+
+    # 次のフレーム、ポイントの準備
+    gray_prev = gray_next.copy()
+    feature_prev = good_next.reshape(-1, 1, 2)
+    end_flag, frame = cap.read()
+
+# 終了処理
+cv2.destroyAllWindows()
+cap.release()
